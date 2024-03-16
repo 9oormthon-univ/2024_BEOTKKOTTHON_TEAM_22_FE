d3781e5e615f84429db8a5659ff798a2893ce869
NohWookJin <jinn98@kakao.com>
2024-03-17 07:31:59 +0900

    feat: Main 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/common/Header';
+import Main from '@/components/Main/Main';
 import Nav from '@/components/common/Nav';
 
 export default function Home() {
   return (
     <main className="relative min-h-screen">
       <Header />
+      <Main />
       <Nav />
     </main>
   );
