b0f6e8defe1e484713850b8d0cb999eaf0c4e0bb
NohWookJin <jinn98@kakao.com>
2024-03-17 07:33:10 +0900

    feat: Nav isActive 기능 추가

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
+'use client';
+
+import { usePathname } from 'next/navigation';
 import { HomeIcon, BookmarkIcon, CommunityIcon, MypageIcon } from './Icons';
 
 const Nav = () => {
+  const pathname = usePathname();
+  const isActive = (path: string) => pathname === path;
+
   return (
-    <nav className="mt-auto">
-      <div className="absolute bottom-0 flex h-[82px] w-[100%] w-full items-center justify-between px-[20px]">
-        <div className="flex flex-col items-center">
-          <HomeIcon color="gray" />
-          <span className="text-gray pt-[3px] text-[15px] ">홈</span>
+    <nav className="sticky bottom-0 z-50 bg-[#fff]">
+      <div className="flex h-[82px] w-[100%] w-full items-center justify-between px-[23px]">
+        <div className="flex flex-col items-center py-[14px]">
+          <HomeIcon color={isActive('/') ? '#3fe0d1' : '#7d7d7d'} />
+          <span className="text-gray pt-[3px] text-[13px]">홈</span>
         </div>
         <div className="flex flex-col items-center">
-          <BookmarkIcon color="gray" />
-          <span className="text-gray pt-[3px] text-[15px]">북마크</span>
+          <BookmarkIcon color={isActive('/like') ? '#3fe0d1' : '#7d7d7d'} />
+          <span className="text-gray pt-[6px] text-[13px]">북마크</span>
         </div>
         <div className="flex flex-col items-center">
-          <CommunityIcon color="gray" />
-          <span className="text-gray pt-[3px] text-[15px]">커뮤니티</span>
+          <CommunityIcon
+            color={isActive('/community') ? '#3fe0d1' : '#7d7d7d'}
+          />
+          <span className="text-gray pt-[6px] text-[13px]">커뮤니티</span>
         </div>
         <div className="flex flex-col items-center">
-          <MypageIcon color="gray" />
-          <span className="text-gray pt-[3px] text-[15px]">마이페이지</span>
+          <MypageIcon color={isActive('/mypage') ? '#3fe0d1' : '#7d7d7d'} />
+          <span className="text-gray pt-[6px] text-[13px]">마이페이지</span>
         </div>
       </div>
     </nav>
