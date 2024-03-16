eb6ed2a9d1e4647d2e5d3fd50bf7dad86bae73e4
NohWookJin <jinn98@kakao.com>
2024-03-17 07:32:29 +0900

    style: Header Css 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,7 +3,7 @@ import { LogoIcon, SearchIcon, AlertIcon } from './Icons';
 
 const Header = () => {
   return (
-    <header className="flex h-[68px] w-[100%] items-center justify-between px-[20px]">
+    <header className="flex h-[68px] w-[100%] items-center justify-between px-[24px]">
       <LogoIcon />
       <div className="flex items-center gap-[20px]">
         <SearchIcon />
