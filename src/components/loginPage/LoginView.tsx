c41f2944dbba255cc80ad393923b4affba5ebd9a
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-03-19 00:24:35 +0900

    feat: 로그인페이지 마크업

--- src/components/loginPage/LoginView.tsx
+++ src/components/loginPage/LoginView.tsx
@@ -1,6 +1,8 @@
 'use client';
 import {useRouter} from "next/navigation";
 import {XIcon} from "@/components/common/Icons";
+import React from 'react';
+import Image from 'next/image';
 
 export default function LoginView() {
   const router = useRouter()
@@ -9,11 +11,31 @@ export default function LoginView() {
     <div className={'px-[24px]'}>
       <div className={'pt-6 , pb-8'}>
         <button onClick={() => router.push('/')}>
-          <XIcon/>
+          <XIcon />
         </button>
       </div>
-      <div>
-        집안일 요정 로고
+      <div className={'mt-32 flex'}>
+        <Image className={'ml-[133px] mr-[133px]'} src={'/images/Logo.svg'} alt={'public/images/Logo.svg'} width={116} height={116}/>
+      </div>
+      <div className={'mt-16 mb-8'}>
+        <input className='w-[100%] h-16 rounded-[12px] border-[1px] border-lightGray'
+               type='text'
+               id={'email'}
+               placeholder={'  이메일을 입력해주세요.'}
+        />
+      </div>
+      <div className={'mt-8 mb-8'}>
+        <input className='w-[100%] h-16 rounded-[12px] border-[1px] border-lightGray'
+               type='text'
+               id={'email'}
+               placeholder={'  비밀번호를 입력해주세요.'}
+        />
+      </div>
+      <div className={'mb-8'}>
+        <button type={'submit'}
+                className={'w-[100%] text-white bg-mint rounded-[12px] h-16 hover:bg-lightGray'}>
+          로그인
+        </button>
       </div>
     </div>
 
