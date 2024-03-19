5afb9976ec6ed708955b5b1dae614a4353560389
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-03-20 04:04:10 +0900

    feat: 마이페이지 마크업, 로그아웃 마크업, 팝업 기능 추가

---
+++ src/components/myPage/UserPoint.tsx
@@ -0,0 +1,12 @@
+export default function UserPoint() {
+
+  return (
+    <>
+      <div className={'px-[17px] mt-[36px] flex items-center w-full h-[100px] bg-mint text-white rounded-[12px]'}>
+        <p className={'flex-1 font-semibold text-[20px]'}>보유 포인트</p>
+        <p className={'font-semibold text-[20px]'}>0000 P</p>
+      </div>
+    </>
+
+  );
+}
