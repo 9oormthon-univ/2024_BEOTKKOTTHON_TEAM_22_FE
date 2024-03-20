f788604e8511e37cdf98537c39f3e0774ffab052
NohWookJin <jinn98@kakao.com>
2024-03-21 00:48:46 +0900

    feat: community page 생성

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,12 +1,17 @@
 import Header from '@/components/common/Header';
+import Community from '@/components/community/Community';
 import Nav from '@/components/common/Nav';
 import { RightArrowIcon } from '@/components/common/Icons';
 
-export default async function Commuity() {
+import { getCommunityItems } from '@/apis/getCommunityItems';
+
+export default async function CommunityPage() {
+  const res = await getCommunityItems();
+
   return (
     <>
       <Header title="Q&A" />
-      <div className="mx-[23px] my-[20px] flex h-[82px] w-[382px] cursor-pointer items-center justify-between rounded-[12px] bg-[#3ddacb] px-[16px]">
+      <section className="mx-[23px] my-[20px] flex h-[82px] w-[382px] cursor-pointer items-center justify-between rounded-[12px] bg-[#3ddacb] px-[16px]">
         <div className="flex flex-col ">
           <span className="text-[14px] text-[#fff]">원하는 정보가 없다면?</span>
           <span className="text-[20px] font-semibold text-[#fff]">
@@ -14,7 +19,11 @@ export default async function Commuity() {
           </span>
         </div>
         <RightArrowIcon />
-      </div>
+      </section>
+      <section className="min-h-screen px-[24px]">
+        {res && <Community communites={res} />}
+      </section>
+
       <Nav />
     </>
   );
