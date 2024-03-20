526ddc186b72bfe885cae46304c815aa31cea22f
NohWookJin <jinn98@kakao.com>
2024-03-21 00:56:36 +0900

    feat: 로고 클릭 기능

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { LogoIcon, SearchIcon, AlertIcon } from './Icons';
 
 interface HeaderProps {
@@ -8,7 +9,9 @@ interface HeaderProps {
 const Header = ({ title }: HeaderProps) => {
   return (
     <header className="flex h-[68px] w-[100%] items-center justify-between px-[24px]">
-      <LogoIcon />
+      <Link href="/" className="cursor-pointer">
+        <LogoIcon />
+      </Link>
       {title && (
         <div>
           <span className="text-[20px] font-semibold">{title}</span>
