1cefda2cfa2ec0f349cc0b7ab9c0a76db3635df1
NohWookJin <jinn98@kakao.com>
2024-03-21 00:49:15 +0900

    refactor: main page 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Header from '@/components/common/Header';
-import Main from '@/components/Main/Main';
+import Main from '@/components/main/Main';
 import Nav from '@/components/common/Nav';
 
 import { getRankingTips } from '@/apis/getRankingTips';
