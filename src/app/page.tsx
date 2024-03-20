8c71730c1c4897fcdad064c28483da6eb30efe5c
NohWookJin <jinn98@kakao.com>
2024-03-21 02:30:08 +0900

    style: 최소 높이 지정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
       <Header />
       <section className="min-h-screen px-[24px]">
         <h1 className="pt-[20px] text-[24px] font-semibold">
-          집안일 요정이 알려드려요!
+          집안일요정이 알려드려요
         </h1>
         {res && <Main tips={res} />}
       </section>
