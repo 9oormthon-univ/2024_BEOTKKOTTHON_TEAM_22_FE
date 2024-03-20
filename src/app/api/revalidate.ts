2f4bbf6fe9604c0bc257f58904b52531a680b02e
NohWookJin <jinn98@kakao.com>
2024-03-21 01:00:18 +0900

    feat: revalidate 태그 함수 추가

---
+++ src/app/api/revalidate.ts
@@ -0,0 +1,10 @@
+'use server';
+
+// server component에서 호출하는 api를
+// client component에서 리렌더링하기 위해 사용.
+
+import { revalidateTag } from 'next/cache';
+
+export const fromClientRevalidateTag = (tagName: string) => {
+  revalidateTag(tagName);
+};
