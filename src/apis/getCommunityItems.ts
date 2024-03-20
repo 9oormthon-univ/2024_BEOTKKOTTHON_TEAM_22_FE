555f5d2f2f3250a54c4a1423523e0bace0be4447
NohWookJin <jinn98@kakao.com>
2024-03-21 00:59:21 +0900

    feat: 상세페이지 api 추가

---
+++ src/apis/getCommunityItems.ts
@@ -0,0 +1,31 @@
+export interface Response {
+  response: Community[];
+}
+
+export interface Community {
+  id: number;
+  category: string;
+  title: string;
+  image_url: string;
+  bookmark_count: number;
+  comment_count: number;
+  timestamp: string;
+}
+
+export const getCommunityItems = async (): Promise<Response> => {
+  try {
+    const res = await fetch(
+      `https://user1710776235315.requestly.tech/community/items`,
+    );
+
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await res.json();
+
+    return data;
+  } catch (err) {
+    throw new Error('Failed to fetch getCommunityItems');
+  }
+};
