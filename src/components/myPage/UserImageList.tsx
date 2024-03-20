c20e66637fb72039d7cb841bab7d466b3ddecd33
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-03-21 01:55:58 +0900

    feat: 유저 프로필 리스트 만들기

---
+++ src/components/myPage/UserImageList.tsx
@@ -0,0 +1,27 @@
+
+export const UserImageList =[
+  {
+    image : '/images/인간.svg',
+    grade : '인간'
+  },
+  {
+    image : '/images/고블린.svg',
+    grade : '고블린'
+  },
+  {
+    image : '/images/도비.svg',
+    grade : '도비'
+  },
+  {
+    image : '/images/요정.svg',
+    grade : '요정'
+  }
+  ,{
+    image : '/images/엘프.svg',
+    grade : '엘프'
+  },
+  {
+    image : '/images/요정왕.svg',
+    grade : '요정왕'
+  }
+]
