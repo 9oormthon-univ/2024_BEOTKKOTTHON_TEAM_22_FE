504b3995455bcacb28e160f390dc68d8fe38cfea
NohWookJin <jinn98@kakao.com>
2024-03-21 19:04:48 +0900

    refactor: Link 태그 위치 변경

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,8 +1,8 @@
+import Link from 'next/link';
 import Header from '@/components/common/Header';
 import Community from '@/components/community/Community';
 import Nav from '@/components/common/Nav';
 import { RightArrowIcon } from '@/components/common/Icons';
-
 import { getCommunityItems } from '@/apis/getCommunityItems';
 
 export default async function CommunityPage() {
@@ -11,7 +11,10 @@ export default async function CommunityPage() {
   return (
     <div className="min-h-screen">
       <Header title="Q&A" />
-      <section className="mx-[23px] my-[20px] flex h-[82px] w-[382px] cursor-pointer items-center justify-between rounded-[12px] bg-[#3ddacb] px-[16px]">
+      <Link
+        href="/community/question"
+        className="mx-[23px] my-[20px] flex h-[82px] w-[382px] cursor-pointer items-center justify-between rounded-[12px] bg-[#3ddacb] px-[16px]"
+      >
         <div className="flex flex-col ">
           <span className="text-[14px] text-[#fff]">원하는 정보가 없다면?</span>
           <span className="text-[20px] font-semibold text-[#fff]">
@@ -19,7 +22,7 @@ export default async function CommunityPage() {
           </span>
         </div>
         <RightArrowIcon />
-      </section>
+      </Link>
       <section className="min-h-screen px-[24px]">
         {res && <Community communites={res} />}
       </section>
