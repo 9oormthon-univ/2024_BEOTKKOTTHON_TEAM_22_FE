e618541b49baf6928e8bcecb267eb52af42525e9
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-03-21 22:55:28 +0900

    feat: 마이페이지 유저정보 조회 api 추가- 수정필요

--- src/components/myPage/MapageView.tsx
+++ src/components/myPage/MapageView.tsx
@@ -1,19 +1,47 @@
+'use client';
+import { useEffect, useState } from 'react';
 import Header from '@/components/common/Header';
 import UserInfo from '@/components/myPage/UserInfo';
 import Nav from '@/components/common/Nav';
 import UserPoint from '@/components/myPage/UserPoint';
 import CheckList from '@/components/myPage/CheckList';
+import axios from 'axios';
+import LoginPopup from '@/components/common/LoginPopup';
+
+interface UserData {
+  nickname: string;
+  totalpoint: number;
+  grade: string;
+}
+
 
 export default function MypageView() {
+  const [userData, setUserData] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    // API 호출
+    axios.get('/mypage')
+      .then(response => {
+        const responseData = response.data.response;
+        setUserData(responseData); // 데이터 설정
+        console.log('api 받아오기',userData)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <>
       <Header title={'마이페이지'}/>
-      <div className={'px-[24px]'}>
-        <UserInfo/>
-        <UserPoint/>
-        <CheckList/>
-        <Nav/>
-      </div>
+      {userData && (
+        <div className={'px-[24px]'}>
+          <UserInfo nickname={userData.nickname} grade={userData.grade}/>
+          <UserPoint point={userData.totalpoint} />
+          <CheckList/>
+          <Nav />
+        </div>
+      )}
     </>
 
   );
